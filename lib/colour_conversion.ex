da5e80cf94d48c86a79a201af916b1b551f61251
Kerry Buckley <kerryjbuckley@gmail.com>
2020-07-14 11:49:17 +0100

    Convert valid input in both directions

--- lib/colour_conversion.ex
+++ lib/colour_conversion.ex
@@ -1,5 +1,11 @@
 defmodule ColourConversion do
-  def convert(a) do
-    a
+  use Bitwise
+
+  def convert(%{r: r, g: g, b: b}) do
+    "##{((r <<< 16) + (g <<< 8) + b) |> Integer.to_string(16) |> String.pad_leading(6, "0")}"
+  end
+
+  def convert(<<"#", r::binary-size(2), g::binary-size(2), b::binary-size(2)>>) do
+    %{r: String.to_integer(r, 16), g: String.to_integer(g, 16),  b: String.to_integer(b, 16)}
   end
 end
--- test/colour_conversion_test.exs
+++ test/colour_conversion_test.exs
@@ -11,5 +11,27 @@ defmodule ColourConversionTest do
         assert rgb |> convert() |> convert() == rgb
       end
     end
+
+    property "converts rgb to hex" do
+      forall {r, g, b} <- {byte(), byte(), byte()} do
+        rgb = %{r: r, g: g, b: b}
+        hex = "##{to_hex(r)}#{to_hex(g)}#{to_hex(b)}"
+        assert convert(rgb) == hex
+      end
+    end
+
+    property "converts hex to rgb" do
+      forall {r, g, b} <- {byte(), byte(), byte()} do
+        hex = "##{to_hex(r)}#{to_hex(g)}#{to_hex(b)}"
+        rgb = %{r: r, g: g, b: b}
+        assert convert(hex) == rgb
+      end
+    end
+  end
+
+  defp to_hex(integer) do
+    integer
+    |> Integer.to_string(16)
+    |> String.pad_leading(2, "0")
   end
 end
