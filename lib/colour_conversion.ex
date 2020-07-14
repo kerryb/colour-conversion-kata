e9f399eed80a22e2e7601191d6bafb98084cd095
Kerry Buckley <kerryjbuckley@gmail.com>
2020-07-14 15:18:24 +0100

    Reject invalid rgb values

--- lib/colour_conversion.ex
+++ lib/colour_conversion.ex
@@ -1,12 +1,15 @@
 defmodule ColourConversion do
   use Bitwise
 
-  def convert(%{r: r, g: g, b: b}) do
+  def convert(%{r: r, g: g, b: b}) when r >= 0 and r <= 255 and g >= 0 and g <= 255 and b >= 0 and b <= 255 do
     "##{((r <<< 16) + (g <<< 8) + b) |> Integer.to_string(16) |> String.pad_leading(6, "0")}"
   end
 
   def convert(<<"#", hex::binary>>), do: convert(hex)
+
   def convert(<<r::binary-size(2), g::binary-size(2), b::binary-size(2)>>) do
     %{r: String.to_integer(r, 16), g: String.to_integer(g, 16), b: String.to_integer(b, 16)}
   end
+
+  def convert(_), do: "Not valid input"
 end
--- test/colour_conversion_test.exs
+++ test/colour_conversion_test.exs
@@ -27,6 +27,7 @@ defmodule ColourConversionTest do
         assert convert(hex) == rgb
       end
     end
+
     property "converts hex (without a leading hash) to rgb" do
       forall {r, g, b} <- {byte(), byte(), byte()} do
         hex = "#{to_hex(r)}#{to_hex(g)}#{to_hex(b)}"
@@ -34,6 +35,31 @@ defmodule ColourConversionTest do
         assert convert(hex) == rgb
       end
     end
+
+    property "returns an error message for invalid red values" do
+      forall {r, g, b} <- {non_byte_integer(), byte(), byte()} do
+        rgb = %{r: r, g: g, b: b}
+        assert convert(rgb) == "Not valid input"
+      end
+    end
+
+    property "returns an error message for invalid green values" do
+      forall {r, g, b} <- {byte(), non_byte_integer(), byte()} do
+        rgb = %{r: r, g: g, b: b}
+        assert convert(rgb) == "Not valid input"
+      end
+    end
+
+    property "returns an error message for invalid blue values" do
+      forall {r, g, b} <- {byte(), byte(), non_byte_integer()} do
+        rgb = %{r: r, g: g, b: b}
+        assert convert(rgb) == "Not valid input"
+      end
+    end
+  end
+
+  defp non_byte_integer do
+    oneof([integer(:inf, -1), integer(256, :inf)])
   end
 
   defp to_hex(integer) do
